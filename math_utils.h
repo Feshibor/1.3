1d59dd921c4b20d0ca522e0f0e40e38c54530619
Feshibor <123161809+Feshibor@users.noreply.github.com>
2025-09-27 16:46:23 +0300

    Delete math_utils.h

--- math_utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
-
-int Kratno(int Num1, int Num2);
-int Equation(double Epsi, double Num1, double Num2, double Num3, double *x1, double *x2);
-int Triangle(double Epsi, double Num1, double Num2, double Num3);
-
-#endif
